4c48114585c3badc08e13169053a38ecd14ad4b8
jkamiya5 <jkamiya5@gmail.com>
2016-04-26 22:21:17 +0900

    実装

---
+++ src/main/java/controller/IndexController.java
@@ -0,0 +1,50 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/")
+	public String indexDirect(Model model) {
+		return "index";
+	}
+	
+	@RequestMapping("/index")
+	public String indexMenubar(Model model) {
+		return "index";
+	}
+
+	@RequestMapping("/info")
+	public String info(Model model) {
+		return "info";
+	}
+	
+	@RequestMapping("/login")
+	public String login(Model model) {
+		return "login";
+	}
+
+	@RequestMapping("/shopping")
+	public String shopping(Model model) {
+		return "shopping";
+	}
+
+	@RequestMapping("/soldout")
+	public String soldout(Model model) {
+		return "soldout";
+	}
+
+	@RequestMapping("/access")
+	public String access(Model model) {
+		return "access";
+	}
+
+	@RequestMapping("/link")
+	public String link(Model model) {
+		return "link";
+	}
+
+}
