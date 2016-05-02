62ed8e9bd49b3773ca6e0501f57f52a0b9ff6948
wordconverter <kamiyaj@stationp.co.jp>
2016-05-03 00:12:41 +0900

    コミット

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import controller.DemoApplication;
+import main.DemoApplication;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DemoApplication.class)
